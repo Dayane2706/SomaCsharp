5bb6c7998ac2bcc40c43b9942878eb8553265e3d
Dayane2706 <dayane263@outlook.com>
2024-03-05 14:35:58 -0300

    Adicionar arquivos de projeto.

---
+++ SomaCsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SomaCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 6;
+            int numero2 = 2;
+
+            Console.WriteLine("Quanto é " + numero1 + " + " + numero2 + "?");
+
+            int reposta = Convert.ToInt32(Console.ReadLine());
+
+            //Estrutura condicional
+
+            if (reposta == 8)
+            {
+                Console.WriteLine("Parabéns! Você acertou!");
+            }
+            else Console.WriteLine("Resposta Errada! Tente Novamente!");
+        }
+    }
+}
+
+
+
